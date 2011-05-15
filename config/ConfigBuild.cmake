7a689cf283e9c22b0c3ba893e6a968f2bd707ce0
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-05-15 19:36:01 +0000

    Modified BASISConfig.cmake configuration.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@430 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ config/ConfigBuild.cmake
@@ -0,0 +1,23 @@
+##############################################################################
+# \file  ConfigBuild.cmake
+# \brief Default configuration of <Project>Config.cmake of build tree.
+#
+# Use the _CONFIG suffix for variables that are replaced in Config.cmake.in.
+#
+# Copyright (c) 2011 University of Pennsylvania. All rights reserved.
+# See LICENSE or Copyright file in project root directory for details.
+#
+# Contact: SBIA Group <sbia-software -at- uphs.upenn.edu>
+##############################################################################
+
+
+# CMake module path
+set (MODULE_PATH_CONFIG "${PROJECT_SOURCE_DIR}/src/cmake")
+
+# include directories
+set (INCLUDE_DIR_CONFIG "")
+
+if (INCLUDE_DIR_CONFIG)
+  list (REMOVE_DUPLICATES INCLUDE_DIR_CONFIG)
+endif ()
+
