d62062e4f9838bdaf65d29011b2afcca2fbafa3e
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-05-16 16:13:14 +0000

    Merged BasisDirectories.cmake and BasisSettings.cmake. The latter is now the only module that sets most variable. Exceptions are options related to specific modules only such as MATLAB Compiler settings.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@436 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- src/cmake/BasisSettings.cmake
+++ src/cmake/BasisSettings.cmake
@@ -10,6 +10,12 @@
 # Moreover, this file specifies the common CMake settings such as the build
 # configuration used by projects following BASIS.
 #
+# \attention Be careful when caching any of the variables. Usually, this
+#            file is included in the root CMake configuration file of the
+#            project. The variables set by this module should only be valid
+#            underneath this directory tree, but not propagate to the parent
+#            or a sibling.
+#
 # \note As this file also sets the CMake policies to be used, it has to
 #       be included using the NO_POLICY_SCOPE in order for these policies
 #       to take effect also in the including file and its subdirectories.
@@ -174,7 +180,135 @@ set (BASIS_TARGETS "" CACHE INTERNAL "${BASIS_TARGETS_DOC}" FORCE)
 # project directory structure
 # ============================================================================
 
-include ("${CMAKE_CURRENT_LIST_DIR}/BasisDirectories.cmake")
+# ----------------------------------------------------------------------------
+# instantiation for specific project
+# ----------------------------------------------------------------------------
+
+# ****************************************************************************
+# \brief Instantiates the project directory structure.
+#
+# This macro is invoked after the CMake project () command to instantiate
+# the project directory structure, i.e., turn the directories into absolute
+# paths using the CMake variables PROJECT_SOURCE_DIR and PROJECT_BINARY_DIR.
+# Moreover, the occurences of @PROJECT_NAME@, @PROJECT_NAME_LOWER@,
+# @PROJECT_NAME_UPPER@, @PROJECT_VERSION@, @PROJECT_VERSION_MAJOR@,
+# @PROJECT_VERSION_MINOR@, and @PROJECT_VERSION_PATH@ are substituted by the
+# actual values corresponding to the project name and version.
+
+macro (basis_initialize_directories)
+  # source tree
+  foreach (P CONFIG DATA DOC EXAMPLE CODE TESTING)
+    set (VAR PROJECT_${P}_DIR)
+    if (NOT IS_ABSOLUTE "${${VAR}}")
+      set (${VAR} "${PROJECT_SOURCE_DIR}/${${VAR}}")
+    endif ()
+  endforeach ()
+
+  # build tree
+  foreach (P RUNTIME LIBRARY ARCHIVE)
+    set (VAR CMAKE_${P}_OUTPUT_DIRECTORY)
+    if (NOT IS_ABSOLUTE "${${VAR}}")
+      set (${VAR} "${PROJECT_BINARY_DIR}/${${VAR}}")
+    endif ()
+  endforeach ()
+
+  # install tree
+  string (CONFIGURE "${INSTALL_PREFIX}" INSTALL_PREFIX @ONLY)
+
+  set (
+    CMAKE_INSTALL_PREFIX "${INSTALL_PREFIX}"
+    CACHE INTERNAL "Installation directories prefix." FORCE
+  )
+
+  foreach (P BIN LIB INCLUDE SHARE DOC DATA EXAMPLE MAN)
+    set (VAR INSTALL_${P}_DIR)
+    string (CONFIGURE "${${VAR}}" ${VAR} @ONLY)
+  endforeach ()
+endmacro ()
+
+# ----------------------------------------------------------------------------
+# source tree
+# ----------------------------------------------------------------------------
+
+# The directories of the source tree are given here relative to the root
+# directory of the project or corresponding subtree, respectively.
+#
+# \note The project template must follow this directory structure.
+#       Ideally, when changing the name of one of these directories,
+#       only the directory structure of the tempate needs to be updated.
+#       The BASIS CMake functions should not be required to change as they
+#       are supposed to use these variables instead of the actual names.
+
+set (PROJECT_CODE_DIR    "src")
+set (PROJECT_CONFIG_DIR  "config")
+set (PROJECT_DATA_DIR    "data")
+set (PROJECT_DOC_DIR     "doc")
+set (PROJECT_EXAMPLE_DIR "example")
+set (PROJECT_TESTING_DIR "test")
+
+set (TESTING_INPUT_DIR    "data")
+set (TESTING_EXPECTED_DIR "expected")
+
+# ----------------------------------------------------------------------------
+# build tree
+# ----------------------------------------------------------------------------
+
+# These directory paths will be made absolute by the initialization functions.
+
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY "bin")
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY "bin")
+set (CMAKE_ARCHIVE_OUTPUT_DIRECTORY "lib")
+
+# ----------------------------------------------------------------------------
+# install tree
+# ----------------------------------------------------------------------------
+
+# In order for CPack to work correctly, the destination paths have to be given
+# relative (to CMAKE_INSTALL_PREFIX). Therefore, the INSTALL_PREFIX prefix is
+# excluded from the following paths. Instead, CMAKE_INSTALL_PREFIX is set to
+# INSTALL_PREFIX. This has to be done after the project attributes are known.
+# Hence, it is done by basis_project (), which configures the variables below.
+
+if (WIN32)
+  set (
+    INSTALL_PREFIX "C:\\Program Files\\SBIA\\\${PROJECT_NAME}"
+    CACHE PATH "Installation directory prefix."
+  )
+
+  set (
+    INSTALL_SINFIX ""
+    CACHE PATH "Installation directories suffix (or infix, respectively)."
+  )
+else ()
+  set (
+    INSTALL_PREFIX "/usr/local"
+    CACHE PATH "Installation directories prefix."
+  )
+
+  set (
+    INSTALL_SINFIX "sbia/@PROJECT_NAME_LOWER@"
+    CACHE PATH "Installation directories suffix (or infix, respectively)."
+  )
+endif ()
+
+set (INSTALL_BIN_DIR     "bin")
+set (INSTALL_INCLUDE_DIR "include")
+set (INSTALL_LIB_DIR     "lib")
+set (INSTALL_SHARE_DIR   "share")
+
+if (INSTALL_SINFIX)
+  set (INSTALL_BIN_DIR       "${INSTALL_BIN_DIR}/${INSTALL_SINFIX}")
+  if (NOT INSTALL_SINFIX STREQUAL "sbia/@PROJECT_NAME_LOWER@")
+    set (INSTALL_INCLUDE_DIR "${INSTALL_INCLUDE_DIR}/${INSTALL_SINFIX}")
+  endif ()
+  set (INSTALL_LIB_DIR       "${INSTALL_LIB_DIR}/${INSTALL_SINFIX}")
+  set (INSTALL_SHARE_DIR     "${INSTALL_SHARE_DIR}/${INSTALL_SINFIX}")
+endif ()
+
+set (INSTALL_DOC_DIR     "${INSTALL_SHARE_DIR}/doc")
+set (INSTALL_DATA_DIR    "${INSTALL_SHARE_DIR}/data")
+set (INSTALL_EXAMPLE_DIR "${INSTALL_SHARE_DIR}/example")
+set (INSTALL_MAN_DIR     "${INSTALL_SHARE_DIR}/man")
 
 # ============================================================================
 # build configuration(s)
