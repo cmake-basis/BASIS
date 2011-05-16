4be397033d55d44c7301850b6976b0ce67178c2f
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-05-16 19:59:00 +0000

    Added ConfigBuild.cmake and ConfigInstall.cmake files to template.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@442 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- data/template/config/ConfigBuild.cmake
+++ data/template/config/ConfigBuild.cmake
@@ -1,20 +1,16 @@
 ##############################################################################
 # \file  ConfigBuild.cmake
-# \brief Default configuration of <Project>Config.cmake of build tree.
+# \brief Default configuration of <project>Config.cmake of build tree.
 #
 # Use the _CONFIG suffix for variables that are replaced in Config.cmake.in.
 #
 # \note The default build tree configuration is included prior to this file.
 #
 # Copyright (c) 2011 University of Pennsylvania. All rights reserved.
-# See LICENSE or Copyright file in project root directory for details.
+# See LICENSE file in project root or 'doc' directory for details.
 #
 # Contact: SBIA Group <sbia-software -at- uphs.upenn.edu>
 ##############################################################################
 
-# CMake module path
-set (MODULE_PATH_CONFIG "${PROJECT_CODE_DIR}/cmake")
-
-# URL of project template
-set (TEMPLATE_URL_CONFIG "${PROJECT_DATA_DIR}/template")
+set (DATA_DIR_CONFIG "${PROJECT_DATA_DIR}")
 
---
+++ data/template/config/ConfigInstall.cmake
@@ -0,0 +1,16 @@
+##############################################################################
+# \file  ConfigInstall.cmake
+# \brief Default configuration of <project>Config.cmake of install tree.
+#
+# Use the _CONFIG suffix for variables that are replaced in Config.cmake.in.
+#
+# \note The default install tree configuration is included prior to this file.
+#
+# Copyright (c) 2011 University of Pennsylvania. All rights reserved.
+# See LICENSE file in project root or 'doc' directory for details.
+#
+# Contact: SBIA Group <sbia-software -at- uphs.upenn.edu>
+##############################################################################
+
+basis_set_config_path (DATA_DIR_CONFIG "${INSTALL_DATA_DIR}")
+
