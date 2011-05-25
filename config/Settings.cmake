74fa405535f86dfc132ab96d103d7a320c454b88
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-05-25 05:09:52 +0000

    Added variable INSTALL_MODULES_DIR. Changed installation directory of CMake modules.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@482 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- config/Settings.cmake
+++ config/Settings.cmake
@@ -16,5 +16,18 @@
 # ============================================================================
 
 # installation directory of project template files
-set (INSTALL_TEMPLATE_DIR "${INSTALL_ETC_DIR}/template")
+set (
+  INSTALL_TEMPLATE_DIR
+    "${INSTALL_ETC_DIR}/template"
+  CACHE PATH
+    "Installation directory of project template (relative to INSTALL_PREFIX)."
+)
+
+# installation directory of CMake modules
+set (
+  INSTALL_MODULES_DIR
+    "share/cmake/${PROJECT_NAME_LOWER}"
+  CACHE PATH
+    "Installation directory of CMake modules (relative to INSTALL_PREFIX)."
+)
 
