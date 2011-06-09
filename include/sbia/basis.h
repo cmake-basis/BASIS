f6bc73ae14609445f166f722aa66532965591c7c
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-06-09 20:23:37 +0000

    Added main include file for C++ utilities.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@533 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ include/sbia/basis.h
@@ -0,0 +1,21 @@
+/*!
+ * \file  basis.h
+ * \brief Main include file of BASIS C++ utilities.
+ *
+ * Copyright (c) 2011 University of Pennsylvania. All rights reserved.
+ * See COPYING file in project root or 'doc' directory for details.
+ *
+ * Contact: SBIA Group <sbia-software -at- uphs.upenn.edu>
+ */
+
+#pragma once
+#ifndef SBIA_BASIS_H_
+#define SBIA_BASIS_H_
+
+
+#include "basis/config.h"
+#include "basis/path.h"
+
+
+#endif // SBIA_BASIS_H_
+
