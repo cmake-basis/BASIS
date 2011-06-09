b4f3dd48d4feb55a3f4db2f2ba2bca008faed875
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-06-09 03:22:01 +0000

    Changed LICENSE to COPYING as the latter is more commonly used, especially by GNU projects. Modified config.h.in and added config.cc.in. Not finished yet, though.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@521 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- src/cmake/FindNiftiCLib.cmake
+++ src/cmake/FindNiftiCLib.cmake
@@ -24,9 +24,9 @@
 #   NiftiCLib_LIBRARIES    Path of niftiio library and prerequisite libraries.
 #
 # Copyright (c) 2011 University of Pennsylvania. All rights reserved.
-# See LICENSE or Copyright file in project root directory for details.
+# See COPYING file in project root or 'doc' directory for details.
 #
-# Contact: SBIA Group <sbia-software@uphs.upenn.edu>
+# Contact: SBIA Group <sbia-software -at- uphs.upenn.edu>
 ##############################################################################
 
 # ============================================================================
