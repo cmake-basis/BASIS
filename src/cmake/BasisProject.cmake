cd80f11a4211698bad5d95523ab6dbf115d85561
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-06-13 22:47:10 +0000

    Append BASIS CMake module path to CMAKE_MODULE_PATH.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@541 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- src/cmake/BasisProject.cmake
+++ src/cmake/BasisProject.cmake
@@ -46,6 +46,9 @@ endif ()
 # modules
 # ============================================================================
 
+# append CMake module path of BASIS to CMAKE_MODULE_PATH
+set (CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${CMAKE_CURRENT_LIST_DIR}")
+
 # The CMakeParseArguments.cmake CMake module was added to CMake since version
 # 2.8.4 which just recenlty was released when the following macros and
 # functions were first implemented. In order to support also previous CMake
