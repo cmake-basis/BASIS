5a0e56d096a11198449140ade6280c7dd425a14f
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-07-04 20:27:42 +0000

    Modified basis_get_matlab_version () and basis_get_matlab_release () such that it sets cache variables if no argument is given. Improved FindMOSEK.cmake module such that non-C/C++ components are optionally searched for.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@655 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- src/cmake/FindMOSEK.cmake
+++ src/cmake/FindMOSEK.cmake
@@ -11,6 +11,13 @@
 #   MOSEK_NO_OMP         Whether to use the link libraries build without
 #                        OpenMP, i.e., multi-threading, enabled. By default,
 #                        the multi-threaded libraries are used.
+#   MOSEK_MATLAB         Whether the MATLAB components of the MOSEK packages
+#                        should be found. Defaults to 1, if MATLAB_FOUND
+#                        evaluates to true and 0 otherwise.
+#   MOSEK_JAVA           Whether the Java components of the MOSEK package should
+#                        be found. Defaults to 0.
+#   MOSEK_PYTHON         Whether the Python components of the MOSEK package
+#                        should be found. Defaults to 0.
 #   MOSEK_TOOLS_SUFFIX   Platform specific path suffix for tools, i.e.,
 #                        "tools/platform/linux64x86" on 64-bit Linux systems.
 #                        If not specified, this module determines the right
@@ -26,9 +33,9 @@
 #                        to determine the extension automatically. Otherwise,
 #                        the MEX extension defaults to "mexa64".
 #   PYTHON_VERSION       Version of Python installation. Set to first two or three
-#                        return values of "sys.version_info". If this variable
-#                        is not set and the basis_get_python_version command is
-#                        available, it is invoked to determine the version
+#                        return values of "sys.version_info" separated by a period (.).
+#                        If this variable is not set and the basis_get_python_version
+#                        command is available, it is invoked to determine the version
 #                        automatically. Otherwise, the Python version defaults to 2.6.
 #
 # Sets the following CMake variables:
@@ -58,34 +65,67 @@ if (NOT MOSEK_DIR)
   set (MOSEK_DIR "$ENV{MOSEK_DIR}" CACHE PATH "Installation prefix for MOSEK.")
 endif ()
 
-# MATLAB version
-if (NOT MATLAB_RELEASE)
-  if (COMMAND basis_get_matlab_release)
-    basis_get_matlab_release (MATLAB_RELEASE)
-  else ()
-    set (MATLAB_RELEASE "R2009b")
+# MATLAB components
+if (NOT DEFINED MOSEK_MATLAB)
+  set (MOSEK_MATLAB ${MATLAB_FOUND})
+endif ()
+
+if (MOSEK_MATLAB)
+  # MATLAB version
+  if (NOT MATLAB_RELEASE)
+    if (COMMAND basis_get_matlab_release)
+      basis_get_matlab_release ()
+      if (NOT MATLAB_RELEASE)
+        message (FATAL_ERROR "Failed to determine release version of MATLAB installation. "
+                             "This information is required to be able to find the right MOSEK MEX-files. "
+                             "Set MATLAB_RELEASE manually and try again.")
+      endif ()
+    else ()
+      set (MATLAB_RELEASE "R2009b")
+    endif ()
+  endif ()
+  # search path for MOSEK MATLAB toolbox
+  if (NOT MOSEK_TOOLBOX_SUFFIX)
+    string (TOLOWER TMP "${MATLAB_RELEASE}")
+    set (MOSEK_TOOLBOX_SUFFIX "toolbox/${TMP}")
+  endif ()
+  # extension of MEX-files
+  if (NOT MEX_EXT)
+    if (COMMAND basis_mexext)
+      basis_mexext (MEX_EXT)
+    else ()
+      set (MEX_EXT "mexa64")
+    endif ()
   endif ()
 endif ()
 
-# extension of MEX-files
-if (NOT MEX_EXT)
-  if (COMMAND basis_mexext)
-    basis_mexext (MEX_EXT)
-  else ()
-    set (MEX_EXT "mexa64")
-  endif ()
+# Java components
+if (NOT DEFINED MOSEK_JAVA)
+  set (MOSEK_JAVA 0)
 endif ()
 
-# Python version
-if (NOT PYTHON_VERSION)
-  if (COMMAND basis_get_python_version)
-    basis_get_python_version (PYTHON_VERSION)
-  else ()
-    set (PYTHON_VERSION "2.6")
-  endif ()
+# Python components
+if (NOT DEFINED MOSEK_PYTHON)
+  set (MOSEK_PYTHON 0)
 endif ()
 
-string (REGEX REPLACE "^([0-9]+)" "\\1" PYTHON_VERSION_MAJOR "${PYTHON_VERSION}")
+if (MOSEK_PYTHON)
+  # Python version
+  if (NOT PYTHON_VERSION)
+    if (COMMAND basis_get_python_version)
+      basis_get_python_version ()
+      if (NOT PYTHON_VERSION)
+        message (FATAL_ERROR "Failed to determine version of Python installation. "
+                             "This information is required to be able to find the right MOSEK Python modules. "
+                             "Set PYTHON_VERSION manually and try again.")
+      endif ()
+    else ()
+      set (PYTHON_VERSION "2.6")
+    endif ()
+  endif ()
+  # major version of Python
+  string (REGEX REPLACE "^([0-9]+)" "\\1" PYTHON_VERSION_MAJOR "${PYTHON_VERSION}")
+endif ()
 
 # library name
 set (MOSEK_LIBRARY_NAME "mosek")
@@ -121,12 +161,6 @@ if (NOT MOSEK_TOOLS_SUFFIX)
   set (MOSEK_TOOLS_SUFFIX "${MOSEK_PATH_SUFFIX}x86")
 endif ()
 
-# search path for MOSEK MATLAB toolbox
-if (NOT MOSEK_TOOLBOX_SUFFIX)
-  string (TOLOWER TMP "${MATLAB_RELEASE}")
-  set (MOSEK_TOOLBOX_SUFFIX "toolbox/${TMP}")
-endif ()
-
 #-------------------------------------------------------------
 # find paths/files
 if (MOSEK_DIR)
@@ -149,32 +183,6 @@ if (MOSEK_DIR)
       NO_DEFAULT_PATH
   )
 
-  find_file (
-    MOSEK_mosekopt_MEX
-      NAMES         mosekopt.${MEX_EXT}
-      HINTS         "${MOSEK_DIR}"
-      PATH_SUFFIXES "${MOSEK_TOOLBOX_SUFFIX}"
-      DOC           "The mosekopt MEX-file of the MOSEK package."
-      NO_DEFAULT_PATH
-  )
-
-  find_file (
-    MOSEK_mosek_JAR
-      NAMES         mosek.jar
-      HINTS         "${MOSEK_DIR}"
-      PATH_SUFFIXES "${MOSEK_TOOLS_SUFFIX}/bin"
-      DOC           "The Java library (.jar file) of the MOSEK package."
-      NO_DEFAULT_PATH
-  )
-
-  find_path (
-    MOSEK_PYTHONPATH
-      NAMES "mosek/array.py"
-      HINTS "${MOSEK_DIR}/${MOSEK_PATH_SUFFIX}/python/${PYTHON_VERSION_MAJOR}"
-      DOC   "Path to MOSEK Python module."
-      NO_DEFAULT_PATH
-  )
-
 else ()
 
   find_path (
@@ -191,41 +199,97 @@ else ()
       DOC   "MOSEK link library."
   )
 
-  find_file (
-    MOSEK_mosekopt_MEX
-      NAMES         mosekopt.${MEX_EXT}
-      PATH_SUFFIXES "${MOSEK_TOOLBOX_SUFFIX}"
-      DOC           "The mosekopt MEX-file of the MOSEK package."
-  )
-
-  find_file (
-    MOSEK_mosek_JAR
-      NAMES mosek.jar
-      HINTS ENV CLASSPATH
-      DOC   "The Java library (.jar file) of the MOSEK package."
-  )
-
-  find_path (
-    MOSEK_PYTHONPATH
-      NAMES "mosek/array.py"
-      HINTS ENV PYTHONPATH
-      DOC   "Path to MOSEK Python module."
-  )
-
 endif ()
 
 mark_as_advanced (MOSEK_INCLUDE_DIR)
 mark_as_advanced (MOSEK_LIBRARY)
-mark_as_advanced (MOSEK_mosekopt_MEX)
-mark_as_advanced (MOSEK_mosek_JAR)
-mark_as_advanced (MOSEK_PYTHONPATH)
 
-if (MOSEK_mosek_JAR)
-  set (MOSEK_CLASSPATH "${MOSEK_mosek_JAR}")
+# MATLAB components
+if (MOSEK_MATLAB)
+  if (MOSEK_DIR)
+
+    find_file (
+      MOSEK_mosekopt_MEX
+        NAMES         mosekopt.${MEX_EXT}
+        HINTS         "${MOSEK_DIR}"
+        PATH_SUFFIXES "${MOSEK_TOOLBOX_SUFFIX}"
+        DOC           "The mosekopt MEX-file of the MOSEK package."
+        NO_DEFAULT_PATH
+    )
+
+  else ()
+
+    find_file (
+      MOSEK_mosekopt_MEX
+        NAMES         mosekopt.${MEX_EXT}
+        PATH_SUFFIXES "${MOSEK_TOOLBOX_SUFFIX}"
+        DOC           "The mosekopt MEX-file of the MOSEK package."
+    )
+
+  endif ()
+
+  if (MOSEK_mosekopt_MEX)
+    set (MOSEK_MEX_FILES "${MOSEK_mosekopt_MEX}")
+  endif ()
+
+  mark_as_advanced (MOSEK_mosekopt_MEX)
 endif ()
 
-if (MOSEK_mosekopt_MEX)
-  set (MOSEK_MEX_FILES "${MOSEK_mosekopt_MEX}")
+# Java components
+if (MOSEK_JAVA)
+  if (MOSEK_DIR)
+
+    find_file (
+      MOSEK_mosek_JAR
+        NAMES         mosek.jar
+        HINTS         "${MOSEK_DIR}"
+        PATH_SUFFIXES "${MOSEK_TOOLS_SUFFIX}/bin"
+        DOC           "The Java library (.jar file) of the MOSEK package."
+        NO_DEFAULT_PATH
+    )
+
+  else ()
+
+    find_file (
+      MOSEK_mosek_JAR
+        NAMES mosek.jar
+        HINTS ENV CLASSPATH
+        DOC   "The Java library (.jar file) of the MOSEK package."
+    )
+
+  endif ()
+
+  if (MOSEK_mosek_JAR)
+    set (MOSEK_CLASSPATH "${MOSEK_mosek_JAR}")
+  endif ()
+
+  mark_as_advanced (MOSEK_mosek_JAR)
+endif ()
+
+# Python components
+if (MOSEK_PYTHON)
+  if (MOSEK_DIR)
+
+    find_path (
+      MOSEK_PYTHONPATH
+        NAMES "mosek/array.py"
+        HINTS ENV PYTHONPATH
+        DOC   "Path to MOSEK Python module."
+    )
+
+  else ()
+
+    find_path (
+      MOSEK_PYTHONPATH
+        NAMES "mosek/array.py"
+        HINTS "${MOSEK_DIR}/${MOSEK_PATH_SUFFIX}/python/${PYTHON_VERSION_MAJOR}"
+        DOC   "Path to MOSEK Python module."
+        NO_DEFAULT_PATH
+    )
+
+  endif ()
+
+  mark_as_advanced (MOSEK_PYTHONPATH)
 endif ()
 
 # ----------------------------------------------------------------------------
@@ -242,14 +306,26 @@ set (MOSEK_INCLUDE_DIRS "${MOSEK_INCLUDES}")
 # if all listed variables are found or TRUE
 include (FindPackageHandleStandardArgs)
 
+set (MOSEK_REQUIRED_VARS
+  MOSEK_INCLUDE_DIR
+  MOSEK_LIBRARY
+)
+
+if (MOSEK_MATLAB)
+  list (APPEND MOSEK_REQUIRED_VARS MOSEK_mosekopt_MEX)
+endif ()
+if (MOSEK_JAVA)
+  list (APPEND MOSEK_REQUIRED_VARS MOSEK_mosek_JAR)
+endif ()
+if (MOSEK_PYTHON)
+  list (APPEND MOSEK_REQUIRED_VARS MOSEK_PYTHONPATH)
+endif ()
+
 find_package_handle_standard_args (
   MOSEK
 # MESSAGE
     DEFAULT_MSG
 # VARIABLES
-    MOSEK_INCLUDE_DIR
-    MOSEK_LIBRARY
-    MOSEK_mosekopt_MEX
-    MOSEK_mosek_JAR
+    ${MOSEK_REQUIRED_VARS}
 )
 
