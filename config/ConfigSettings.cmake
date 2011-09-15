7b4699828c74ce4d09e6c46f6e82ba46cbd39bce
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-09-15 03:49:53 +0000

    Fixed problem with basis_get_target_location() with optional ARGV2 parameter. The parameter is not optional any more because of that (might be a CMake bug). Added function basis_default_doxygen_filters() to solve problem with order of module includes.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@833 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- config/ConfigSettings.cmake
+++ config/ConfigSettings.cmake
@@ -31,11 +31,6 @@ if (BUILD_CONFIG_SETTINGS)
       set (${U}_TEMPLATES_DIR_CONFIG "${PROJECT_CODE_DIR}/utilities/${L}")
     endforeach ()
 
-    # libraries
-    basis_get_target_location (UTILS_LIBRARY_CONFIG     basis_utils)
-    basis_get_target_location (TEST_LIBRARY_CONFIG      basis_test)
-    basis_get_target_location (TEST_MAIN_LIBRARY_CONFIG basis_test_main)
-
     # URL of project template
     set (TEMPLATE_URL_CONFIG "${PROJECT_ETC_DIR}/template")
 
@@ -54,23 +49,6 @@ foreach (L CXX Python Perl BASH)
   string (TOUPPER "${L}" U)
   basis_set_config_path (${U}_TEMPLATES_DIR_CONFIG  "${INSTALL_${U}_TEMPLATES_DIR}")
 endforeach ()
-
-# libraries
-file (
-  RELATIVE_PATH LIB_DIR
-    "${INSTALL_PREFIX}/${INSTALL_CONFIG_DIR}"
-    "${INSTALL_PREFIX}/${INSTALL_ARCHIVE_DIR}"
-)
-string (REGEX REPLACE "/$" "" LIB_DIR "${LIB_DIR}")
-
-basis_get_target_location (UTILS_LIBRARY_CONFIG basis_utils NAME)
-set (UTILS_LIBRARY_CONFIG "\${CMAKE_CURRENT_LIST_DIR}/${LIB_DIR}/${UTILS_LIBRARY_CONFIG}")
-
-basis_get_target_location (TEST_LIBRARY_CONFIG basis_test NAME)
-set (TEST_LIBRARY_CONFIG "\${CMAKE_CURRENT_LIST_DIR}/${LIB_DIR}/${TEST_LIBRARY_CONFIG}")
-
-basis_get_target_location (TEST_MAIN_LIBRARY_CONFIG basis_test_main NAME)
-set (TEST_MAIN_LIBRARY_CONFIG "\${CMAKE_CURRENT_LIST_DIR}/${LIB_DIR}/${TEST_MAIN_LIBRARY_CONFIG}")
 
 # URL of project template
 basis_set_config_path (TEMPLATE_URL_CONFIG "${INSTALL_TEMPLATE_DIR}")
