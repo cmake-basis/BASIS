cb077c4c2dcb31c3c1c017b28b2dd0cfff2b493a
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-10-14 16:06:31 +0000

    Added FindPerlLibs.cmake module which fixes an issue with the version shipped with CMake <= 2.8.5.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1054 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ src/modules/FindPerlLibs.cmake
@@ -0,0 +1,74 @@
+##############################################################################
+# @file  FindPerlLibs.cmake
+# @brief Find Perl libraries. Fixes issue with CMake's default FindPerlLibs.
+#
+# @sa http://www.cmake.org/pipermail/cmake/2008-July/022638.html
+#
+# Copyright (c) 2011 University of Pennsylvania. All rights reserved.
+# See https://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+#
+# @ingroup CMakeFindModules
+##############################################################################
+
+# ----------------------------------------------------------------------------
+# use CMake's FindPerlLibs.cmake module
+set (CMAKE_MODULE_PATH_BACKUP "${CMAKE_MODULE_PATH}")
+set (CMAKE_MODULE_PATH)
+
+find_package (PerlLibs)
+
+set (CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH_BACKUP}")
+set (CMAKE_MODULE_PATH_BACKUP)
+
+# ----------------------------------------------------------------------------
+# try to fix any issues
+if (PERL_EXECUTABLE)
+  execute_process (
+    COMMAND "${PERL_EXECUTABLE}" -MConfig -e "print \$Config{version}"
+    OUTPUT_VARIABLE PERL_OUTPUT
+    RESULT_VARIABLE PERL_RETURN_VALUE
+  )
+  if (PERL_RETURN_VALUE EQUAL 0)
+    set (PERL_VERSION ${PERL_OUTPUT})
+  endif ()
+
+  # try to fix failure in PERL_INCLUDE_PATH
+  if (PERL_INCLUDE_PATH MATCHES ".*-NOTFOUND")
+    execute_process (
+      COMMAND "${PERL_EXECUTABLE}" -MConfig -e "print \$Config{archlibexp}"
+      OUTPUT_VARIABLE PERL_OUTPUT
+      RESULT_VARIABLE PERL_RETURN_VALUE
+    )
+    if (NOT PERL_RETURN_VALUE)
+      find_path (PERL_INCLUDE_PATH perl.h "${PERL_OUTPUT}/CORE")
+    endif ()
+  endif ()
+
+  # try to fix failure in PERL_LIBRARY
+  IF (PERL_LIBRARY MATCHES ".*-NOTFOUND")
+    execute_process (
+      COMMAND "${PERL_EXECUTABLE}" -MConfig -e "print \$Config{libperl}"
+      OUTPUT_VARIABLE PERL_OUTPUT
+      RESULT_VARIABLE PERL_RETURN_VALUE
+    )
+    if (NOT PERL_RETURN_VALUE)
+      find_library (PERL_LIBRARY NAMES "${PERL_OUTPUT}" PATHS "${PERL_INCLUDE_PATH}")
+    endif ()
+  endif ()
+endif ()
+
+# ----------------------------------------------------------------------------
+# handle the QUIETLY and REQUIRED arguments and set *_FOUND to TRUE
+# if all listed variables are found or TRUE
+include (FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args (
+  PerlLibs
+  REQUIRED_VARS
+    PERL_LIBRARY
+    PERL_INCLUDE_PATH
+  VERSION_VAR
+    PERL_VERSION
+)
