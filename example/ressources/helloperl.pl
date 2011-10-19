0531c9b6d845963d5b2c52ec8c875542c1020e8e
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-10-19 22:09:55 +0000

    Added source files used in Getting Started tutorial.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1152 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ example/ressources/helloperl.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/env perl
+
+##############################################################################
+# @file  helloperl.pl
+# @brief The typical Hello World! example program.
+#
+# Copyright (c) University of Pennsylvania. All rights reserved.
+# See https://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+##############################################################################
+
+print "Hello BASIS!\n"
---
+++ example/ressources/hellopython.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+##############################################################################
+# @file  hellopython.py
+# @brief The typical Hello World! example program.
+#
+# Copyright (c) University of Pennsylvania. All rights reserved.
+# See https://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+##############################################################################
+
+if __name__ == '__main__':
+    print "Hello BASIS!"
