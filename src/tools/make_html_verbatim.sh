b7cecc0c2ca5d255dcd5375bd6a83addd21a31a7
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-11-03 21:53:52 +0000

    Added simple tool to convert INSTALL-basis.txt document such that the content can be placed in <pre></pre> HTML tag of the file which can be accessed via the public URL http://www.rad.upenn.edu/sbia/software/installation.html .
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1282 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ src/tools/make_html_verbatim.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+##############################################################################
+# @file  make_html_verbatim.sh
+# @brief Auxiliary script used to convert plain text file such that the
+#        content can be pasted into the body of a HTML <verbatim> tag.
+#
+# Copyright (c) University of Pennsylvania. All rights reserved.
+# See https://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+##############################################################################
+
+if [ $# -eq 0 -o "$1" == "--help" -o "$1" == "--helpshort" -o "$1" == "-h" ]; then
+    echo "Usage: make_html_verbatim <text file>"
+    exit 1
+fi
+
+sed 's/</\&lt;/g;s/>/\&gt;/g' "$1"
