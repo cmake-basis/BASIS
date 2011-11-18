3ff3b33554c8d530147aaf2b1daad6083a1772b5
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-11-18 08:14:49 +0000

    Changed use of BASIS_INCLUDE_DIRECTORIES variables to use of read-only INCLUDE_DIRECTORIES property on directories. Same for LINK_DIRECTORIES. Revised conversion of target names to target UIDs and vice versa. Added first tests for CMake functions.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1346 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ test/cmake/test_string_manipulation.cmake
@@ -0,0 +1,36 @@
+##############################################################################
+# @file  test_basis_target_uid.cmake
+# @brief Test basis_target_uid() and related functions.
+#
+# Copyright (c) University of Pennsylvania. All rights reserved.
+# See https://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+##############################################################################
+
+# ----------------------------------------------------------------------------
+# include modules
+include ("${MODULE_PATH}/CommonTools.cmake")
+
+# ----------------------------------------------------------------------------
+# test basis_sanitize_for_regex()
+basis_sanitize_for_regex (S "module.py")
+if (NOT "${S}" STREQUAL "module\\.py")
+  message (FATAL_ERROR "Expected 'module\\.py' got '${S}'.")
+endif ()
+basis_sanitize_for_regex (S "mo.du+a*")
+if (NOT "${S}" STREQUAL "mo\\.du\\+a\\*")
+  message (FATAL_ERROR "Expected 'mo\\.du\\+a\\*' got '${S}'.")
+endif ()
+basis_sanitize_for_regex (S "this is ^not the beginning")
+if (NOT "${S}" STREQUAL "this is \\^not the beginning")
+  message (FATAL_ERROR "Expected 'this is \\^not the beginning' got '${S}'.")
+endif ()
+basis_sanitize_for_regex (S "not the end$ yet")
+if (NOT "${S}" STREQUAL "not the end\\$ yet")
+  message (FATAL_ERROR "Expected 'not the end\\$ yet' got '${S}'.")
+endif ()
+basis_sanitize_for_regex (S "neither ^start nor end$ match")
+if (NOT "${S}" STREQUAL "neither \\^start nor end\\$ match")
+  message (FATAL_ERROR "Expected 'neither \\^start nor end\\$ match' got '${S}'.")
+endif ()
