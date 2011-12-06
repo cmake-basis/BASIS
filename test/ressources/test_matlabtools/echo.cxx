005d4e110113b3bafd18c22dbf33cff51657d323
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-12-06 21:39:59 +0000

    Added test build of MEX-file.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1505 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ test/ressources/test_matlabtools/echo.cxx
@@ -0,0 +1,35 @@
+/**
+ * @file  echo.cxx
+ * @brief Example MEX-file used to test build of MEX-files.
+ *
+ * Copyright (c) 2011 University of Pennsylvania. All rights reserved.
+ * See https://sbia-svn.uphs.upenn.edu/sbia/software/license.html or COPYING file.
+ *
+ * Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+ */
+
+#include <mex.h>
+
+// ---------------------------------------------------------------------------
+void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
+{
+    // check arguments
+    if (nrhs != 1) {
+        mexErrMsgTxt("Missing message argument or too many arguments given.");
+    }
+    if (nlhs > 0) {
+        mexErrMsgTxt("This function does not return anything.")
+    }
+    if (mxIsChar(prhs[0]) != 1 || mxGetM(prhs[0]) != 1) {
+        mexErrMsgTxt("Argument must be a string.")
+    }
+    // convert argument to string
+    char* msg = mxArrayToString(prhs[0]);
+    if (!msg) {
+        mexErrMsgTxt("Could not convert argument to string.")
+    }
+    // print message
+    fprintf(msg);
+    // clean up
+    mxFree(msg);
+}
