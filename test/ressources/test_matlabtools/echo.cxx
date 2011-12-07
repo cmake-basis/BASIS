9f6b0872366dc8e5327f64bff0d2ef8fbe4be2e1
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-12-07 23:52:27 +0000

    Fixed test MEX-file implementation.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1534 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- test/ressources/test_matlabtools/echo.cxx
+++ test/ressources/test_matlabtools/echo.cxx
@@ -18,15 +18,15 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
         mexErrMsgTxt("Missing message argument or too many arguments given.");
     }
     if (nlhs > 0) {
-        mexErrMsgTxt("This function does not return anything.")
+        mexErrMsgTxt("This function does not return anything.");
     }
     if (mxIsChar(prhs[0]) != 1 || mxGetM(prhs[0]) != 1) {
-        mexErrMsgTxt("Argument must be a string.")
+        mexErrMsgTxt("Argument must be a string.");
     }
     // convert argument to string
     char* msg = mxArrayToString(prhs[0]);
     if (!msg) {
-        mexErrMsgTxt("Could not convert argument to string.")
+        mexErrMsgTxt("Could not convert argument to string.");
     }
     // print message
     fprintf(msg);
