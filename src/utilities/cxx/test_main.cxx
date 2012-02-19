5ecca25f1ce281856b8dc874f2f68486b693cf76
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-02-19 01:22:31 +0000

    Modify tests to output hostname as DartMeasurement.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@1901 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- src/utilities/cxx/test_main.cxx
+++ src/utilities/cxx/test_main.cxx
@@ -39,13 +39,23 @@
 #include <iostream>
 #include <sbia/basis/test.h>
 
+#if defined (_WIN32) || defined (WIN32) || defined (_WINDOWS)
+#  include <Winsock2.h> // gethostbyname()
+#  ifdef max
+#    undef max
+#  endif
+#  pragma comment(lib, "Ws2_32.lib")
+#else
+#  include <netdb.h> // gethostbyname()
+#endif
+
 // MS C++ compiler/linker has a bug on Windows (not on Windows CE), which
 // causes a link error when _tmain is defined in a static library and UNICODE
 // is enabled. For this reason instead of _tmain, main function is used on
 // Windows. See the following link to track the current status of this bug:
 // http://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=394464  // NOLINT
 #if GTEST_OS_WINDOWS_MOBILE
-# include <tchar.h>  // NOLINT
+#  include <tchar.h>  // NOLINT
 
 /**
  * @brief Default implementation of _tmain() function of Google Mock based unit test drivers.
@@ -63,9 +73,23 @@ int _tmain(int argc, TCHAR** argv) {
  */
 int main(int argc, char** argv) {
 #endif  // GTEST_OS_WINDOWS_MOBILE
-  // Since Google Mock depends on Google Test, InitGoogleMock() is
-  // also responsible for initializing Google Test.  Therefore there's
-  // no need for calling testing::InitGoogleTest() separately.
-  testing::InitGoogleMock(&argc, argv);
-  return RUN_ALL_TESTS();
+    // add host name as Dart/CDash measurement
+    char hostname[256] = "unknown";
+    #if defined (_WIN32) || defined (WIN32) || defined (_WINDOWS)
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
+        gethostname(hostname, sizeof(hostname));
+        WSACleanup();
+    #else
+        gethostname(hostname, sizeof(hostname));
+    #endif
+    hostname[255] = '\0';
+    std::cout << "<DartMeasurement name=\"Host Name\" type=\"string\">";
+    std::cout << hostname;
+    std::cout <<  "</DartMeasurement>" << std::endl;
+    // Since Google Mock depends on Google Test, InitGoogleMock() is
+    // also responsible for initializing Google Test.  Therefore there's
+    // no need for calling testing::InitGoogleTest() separately.
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
 }
