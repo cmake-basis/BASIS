cb1d6bf420e07e1eb1f70098d226359f08fa67c4
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-02-28 22:55:55 +0000

    Fix use of get_project_property() in FindOpenCV.cmake.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2033 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- FindOpenCV.cmake
+++ FindOpenCV.cmake
@@ -1,30 +1,32 @@
-###########################################################
-#                  Find OpenCV Library
-# See http://sourceforge.net/projects/opencvlibrary/
-#----------------------------------------------------------
+##############################################################################
+# @file FindOpenCV.cmake
+# @brief Find OpenCV Library (http://sourceforge.net/projects/opencvlibrary/)
 #
-## 1: Setup:
+# @par 1. Setup
+#
 # The following variables are optionally searched for defaults
 #  OpenCV_DIR:            Base directory of OpenCv tree to use.
 #
-## 2: Variable
+# @par 2. Variable
+#
 # The following are set after configuration is done: 
 #  
-#  OpenCV_FOUND
-#  OpenCV_LIBS
-#  OpenCV_INCLUDE_DIR
-#  OpenCV_VERSION (OpenCV_VERSION_MAJOR, OpenCV_VERSION_MINOR, OpenCV_VERSION_PATCH)
+# - OpenCV_FOUND
+# - OpenCV_LIBS
+# - OpenCV_INCLUDE_DIR
+# - OpenCV_VERSION (OpenCV_VERSION_MAJOR, OpenCV_VERSION_MINOR, OpenCV_VERSION_PATCH)
 #
 #
-# Deprecated variable are used to maintain backward compatibility with
-# the script of Jan Woetzel (2006/09): www.mip.informatik.uni-kiel.de/~jw
-#  OpenCV_INCLUDE_DIRS
-#  OpenCV_LIBRARIES
-#  OpenCV_LINK_DIRECTORIES
+# The following variables are used to maintain compatibility with other
+# Find<Pkg>.cmake modules, including the FindOpenCV.cmake module of
+# of Jan Woetzel (2006/09, www.mip.informatik.uni-kiel.de/~jw):
+# - OpenCV_INCLUDE_DIRS
+# - OpenCV_LIBRARIES
+# - OpenCV_LINK_DIRECTORIES
 # 
-## 3: Version
+# @par 3. Version
 #
-# 2012/02/28 Andreas Schuh, Modified for inclusion in BASIS.
+# 2012/02/28 Andreas Schuh, Reimplemented module to work also for OpenCV 1.x.
 # 2010/04/07 Benoit Rat, Correct a bug when OpenCVConfig.cmake is not found.
 # 2010/03/24 Benoit Rat, Add compatibility for when OpenCVConfig.cmake is not found.
 # 2010/03/22 Benoit Rat, Creation of the script.
@@ -34,19 +36,17 @@
 # - OpenCV 2.1:  MinGW, MSVC2008
 # - OpenCV 2.0:  MinGW, MSVC2008, GCC4
 #
-#
-## 4: Licence:
+# @par 4. Licence
 #
 # LGPL 2.1 : GNU Lesser General Public License Usage
 # Alternatively, this file may be used under the terms of the GNU Lesser
-
+#
 # General Public License version 2.1 as published by the Free Software
 # Foundation and appearing in the file LICENSE.LGPL included in the
 # packaging of this file.  Please review the following information to
 # ensure the GNU Lesser General Public License version 2.1 requirements
 # will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-# 
-#----------------------------------------------------------
+##############################################################################
 
 # ----------------------------------------------------------------------------
 # initialize search
@@ -82,7 +82,7 @@ if (EXISTS "${OpenCV_DIR}")
     # Note that OpenCV 2.0.0 does only call the command include_directories()
     # but does not set OpenCV_INCLUDE_DIRS. This variable was added to
     # OpenCVConfig.cmake since version 2.1.0 of OpenCV.
-    get_directory_property (__INCLUDE_DIRS PROPERTY INCLUDE_DIRECTORIES)
+    get_directory_property (__INCLUDE_DIRS INCLUDE_DIRECTORIES)
     find_path (
       OpenCV_INCLUDE_DIR "cv.h"
       HINTS ${__INCLUDE_DIRS}
