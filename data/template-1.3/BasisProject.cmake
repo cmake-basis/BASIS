9ecbe2c47594186295ee317c0914049259ba21a0
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-03-28 16:32:13 +0000

    Change reference to BASIS web site in INSTALL.txt.
    The main BASIS web site is no longer on SourceForge.
    
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2053 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ data/template-1.3/BasisProject.cmake
@@ -0,0 +1,45 @@
+##############################################################################
+# @file  BasisProject.cmake
+# @brief Meta-data of this BASIS project.
+#
+# This file defines project meta-data by calling the basis_project() function.
+# This meta-data is used by BASIS to setup the project. Moreover, if the
+# project is a module of another BASIS project, the dependencies to other
+# modules have to be specified here such that the top-level project can analyze
+# the inter-module dependencies
+# (see page Project Modularization of BASIS documentation).
+#
+# However, not only dependencies to other modules can be specified here,
+# but also dependencies on external packages. A more flexible alternative to
+# resolve external dependencies is to add the corresponding basis_find_package()
+# statements to the Depends.cmake file. This should, however, only be done
+# if specifying the dependencies as arguments to the basis_project() function
+# cannot be used to resolve the dependencies properly. If you only need to
+# make use of additional variables set by the package configuration file
+# of the external package or the corresponding Find<Package>.cmake module,
+# add the related CMake code to the Settings.cmake file instead.
+#
+# Copyright (c) <year>, University of Pennsylvania. All rights reserved.<br />
+# See http://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+#
+# @ingroup BasisSettings
+##############################################################################
+
+basis_project (
+  NAME
+    "<project>"
+  VERSION
+    0.0.0
+  DESCRIPTION
+    "<description>"
+  DEPENDS
+    #<dependency>
+  OPTIONAL_DEPENDS
+    #<optional-dependency>
+  TEST_DEPENDS
+    #<test-dependency>
+  OPTIONAL_TEST_DEPENDS
+    #<optional-test-dependency>
+)
---
+++ data/template-1.3/config/ConfigSettings.cmake
@@ -0,0 +1,35 @@
+##############################################################################
+# @file  ConfigSettings.cmake
+# @brief Sets variables used in CMake package configuration.
+#
+# It is suggested to use @c _CONFIG as suffix for variable names that are to
+# be substituted in the Config.cmake.in template file in order to distinguish
+# these variables from the build configuration.
+#
+# @note The default BasisConfigSettings.cmake file which is part of the BASIS
+#       installation is included prior to this file. Hence, the variables are
+#       valid even if a custom project-specific configuration is used and
+#       default values can further be overwritten in this file.
+#
+# Copyright (c) <year>, University of Pennsylvania. All rights reserved.<br />
+# See http://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+#
+# Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+#
+# @ingroup BasisSettings
+##############################################################################
+
+# ============================================================================
+# build tree configuration settings
+# ============================================================================
+
+if (BUILD_CONFIG_SETTINGS)
+  set (DATA_DIR_CONFIG "${PROJECT_DATA_DIR}")
+  return ()
+endif ()
+
+# ============================================================================
+# installation configuration settings
+# ============================================================================
+
+set (DATA_DIR_CONFIG "${INSTALL_DATA_DIR}")
