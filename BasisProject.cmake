7147eb2accb16516bf5750c7038497a77b4f0a33
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-05-01 16:09:17 +0000

    Change version of trunk back to 0.0.0.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2167 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -54,7 +54,7 @@ basis_project (
   NAME
     "BASIS"
   VERSION
-    1.3.0
+    0.0.0
   DESCRIPTION
     "This package implements and supports the development of "
     "software which follows the SBIA Build system And Software "
