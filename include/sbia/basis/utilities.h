928cde6c253355e11992db0914cbf7b9398a4916
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-06-20 06:36:45 +0000

    Second major revision of C++ utilities.
    The C++ utilities look good now. Better separation of project-independent and project-specific implementations. Reduces number of LoC in basis.cxx noticeably.
    
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2422 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ include/sbia/basis/utilities.h
@@ -0,0 +1,394 @@
+/**
+ * @file  utilities.h
+ * @brief Main module of project-independent BASIS utilities.
+ *
+ * This module defines project-independent implementations of the BASIS utility
+ * functions. They are intended for use outside a BASIS-based project only.
+ * Within source code of a BASIS-based project, the overloads declared by
+ * the basis.h module should be used instead as these are specifically configured
+ * by BASIS for this project during the build of the software.
+ *
+ * Note that this module includes all other BASIS utility header files.
+ * Hence, it is sufficient to include this file only.
+ *
+ * Copyright (c) 2011, 2012 University of Pennsylvania. All rights reserved.<br />
+ * See http://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+ *
+ * Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+ *
+ * @ingroup BasisCxxUtilities
+ */
+
+#ifndef _SBIA_BASIS_UTILITIES_H
+#define _SBIA_BASIS_UTILITIES_H
+
+// standard library
+#include <string>
+#include <iostream>
+#include <vector>
+
+// basic utilities
+#include "config.h"
+#include "assert.h"
+#include "except.h"
+#include "os.h"
+
+// command-line parsing
+#include "CmdLine.h"
+
+
+/// @addtogroup BasisCxxUtilities
+/// @{
+
+namespace sbia {
+
+namespace basis {
+
+
+// ===========================================================================
+// executable information
+// ===========================================================================
+
+/**
+ * @brief Provides information about executable build targets.
+ *
+ * Within source code of a BASIS project, other SBIA executables are called
+ * only indirectly using the build target name which must be fixed and unique
+ * within the lab. The name of the output executable file of these targets may
+ * however vary and be finally set by BASIS, for example, depending on
+ * whether the project is build as part of a superproject or not. Therefore,
+ * each BASIS CMake function may adjust the output name in order to resolve
+ * name conflicts with other targets or SBIA executables.
+ *
+ * The idea is that a target name is supposed to be stable and known to the
+ * developer as soon as the target is added to a CMakeLists.txt file, while
+ * the name of the actual executable file is not known a priori as it is set
+ * by the BASIS CMake functions during the configure step. Thus, the developer
+ * should not rely on a particular name of the executable file. Instead, they
+ * can rely on the name of the corresponding build target which was chosen by
+ * themselves when adding the target to the build configuration.
+ *
+ * In order to get the actual file path of the built executable file, the
+ * function get_executable_path() is provided by the stdaux.h module.
+ * This function uses the static singleton instance of this class in order to
+ * map the given build target name to the name of the built and optionally
+ * installed executable. The code which initializes the required maps is
+ * generated automatically during the configuration of the build system.
+ *
+ * @sa exepath()
+ */
+class IExecutableTargetInfo
+{
+    // -----------------------------------------------------------------------
+    // construction / destruction
+protected:
+
+    /// @brief Constructor
+    IExecutableTargetInfo() {}
+
+    /// @brief Destructor.
+    virtual ~IExecutableTargetInfo() {};
+
+    // -----------------------------------------------------------------------
+    // public interface
+public:
+
+    /**
+     * @brief Get UID of build target.
+     *
+     * In order to be able to distinguish build targets with identical name
+     * but which are built as part of different BASIS projects, the UID of
+     * a build target is composed of the build target name as given as
+     * argument to the basis_add_* CMake functions and a namespace identifier
+     * (i.e., the project name in lowercase letters). If the specified build
+     * target name is neither known by this module nor a build target UID yet,
+     * this method prepends the namespace identifier corresponding to the
+     * project this module was built from, assuming that the caller refers
+     * to another target within the same project.
+     *
+     * @param [in] target Name/UID of build target.
+     *
+     * @returns UID of build target.
+     */
+    virtual std::string targetuid(const std::string& target) const = 0;
+
+    /**
+     * @brief Determine whether a given build target is known.
+     *
+     * @param [in] target Name/UID of build target.
+     *
+     * @returns Whether the given build target is known by this module.
+     */
+    virtual bool istarget(const std::string& target) const = 0;
+
+    /**
+     * @brief Get name of executable file without directory path.
+     *
+     * @param [in] target Name/UID of build target.
+     *
+     * @returns Name of built executable file without path.
+     */
+    virtual std::string basename(const std::string& target) const = 0;
+
+    /**
+     * @brief Get absolute path of directory containing executable.
+     *
+     * @param [in] target Name/UID of build target.
+     *
+     * @returns Absolute path of directory containing executable file.
+     */
+    virtual std::string dirname(const std::string& target) const = 0;
+
+}; // class IExecutableTargetInfo
+
+
+/**
+ * @brief Print contact information.
+ *
+ * @param [in] contact Name of contact.
+ */
+void print_contact(const char* contact);
+
+/**
+ * @brief Print version information including copyright and license notices.
+ *
+ * @param [in] name      Name of executable. Should not be set programmatically
+ *                       to the first argument of the @c_main() function, but
+ *                       a string literal instead.
+ * @param [in] version   Version of executable, e.g., release of project
+ *                       this executable belongs to.
+ * @param [in] project   Name of project this executable belongs to.
+ *                       If @c NULL or an empty string, no project information
+ *                       is printed.
+ * @param [in] copyright The copyright notice, excluding the common prefix
+ *                       "Copyright (c) " and suffix ". All rights reserved.".
+ *                       If @c NULL or an empty string, no copyright notice
+ *                       is printed.
+ * @param [in] license   Information regarding licensing. If @c NULL or an empty
+ *                       string, no license information is printed.
+ */
+void print_version(const char* name,
+                   const char* version,
+                   const char* project   = NULL,
+                   const char* copyright = NULL,
+                   const char* license   = NULL);
+
+/**
+ * @brief Get UID of build target.
+ *
+ * The UID of a build target is its name prepended by a namespace identifier
+ * which should be unique for each project.
+ *
+ * @param [in] name    Name of build target.
+ * @param [in] targets Structure providing information about executable targets.
+ *
+ * @returns UID of named build target.
+ */
+std::string targetuid(const std::string& name, const IExecutableTargetInfo* targets = NULL);
+
+/**
+ * @brief Determine whether a given build target is known.
+ *
+ * @param [in] name    Name of build target.
+ * @param [in] targets Structure providing information about executable targets.
+ *
+ * @returns Whether the named target is a known executable target.
+ */
+bool istarget(const std::string& name, const IExecutableTargetInfo* targets = NULL);
+
+/**
+ * @brief Get absolute path of executable file.
+ *
+ * This function determines the absolute file path of an executable. If no
+ * arguments are given, the absolute path of this executable is returned.
+ * If the command names a known executable build target, the absolute path to
+ * the corresonding built (and installed) executable file is returned.
+ * Otherwise, the named command is searched in the system @c PATH and its
+ * absolute path returned if found. If the executable is not found, an
+ * empty string is returned.
+ *
+ * @todo This function currently makes use of the which command implemented
+ *       in Python and called as subprocess in order to search a command
+ *       in the system @c PATH. This which command is part of BASIS and
+ *       can also be used on Windows. However, a native C++ implementation
+ *       would be desireable.
+ *
+ * @param [in] name    Name of command or @c NULL.
+ * @param [in] targets Structure providing information about executable targets.
+ *
+ * @returns Absolute path of executable or an empty string if not found.
+ *          If @p name is @c NULL, the path of this executable is returned.
+ *
+ * @sa exename()
+ * @sa exedir()
+ */
+std::string exepath(const std::string&           name    = std::string(),
+                    const IExecutableTargetInfo* targets = NULL);
+
+/**
+ * @brief Get name of executable file.
+ *
+ * The name of the executable may or may not include the file name extension
+ * depending on the executable type and operating system. Hence, this function
+ * is neither an equivalent to os::path::basename(exepath()) nor
+ * os::path::filename(exepath()). In particular, on Windows, the .exe and .com
+ * extension is not included in the returned executable name.
+ *
+ * @param [in] name    Name of command or @c NULL.
+ * @param [in] targets Structure providing information about executable targets.
+ *
+ * @returns Name of executable file or an empty string if not found.
+ *          If @p name is @c NULL, the name of this executable is returned.
+ *
+ * @sa exepath()
+ */
+std::string exename(const std::string&           name    = std::string(),
+                    const IExecutableTargetInfo* targets = NULL);
+
+/**
+ * @brief Get directory of executable file.
+ *
+ * @param [in] name    Name of command or @c NULL.
+ * @param [in] targets Structure providing information about executable targets.
+ *
+ * @returns Absolute path of directory containing executable or an empty string if not found.
+ *          If @p name is @c NULL, the directory of this executable is returned.
+ *
+ * @sa exepath()
+ */
+std::string exedir(const std::string&           name    = std::string(),
+                   const IExecutableTargetInfo* targets = NULL);
+
+// ===========================================================================
+// command execution
+// ===========================================================================
+
+/**
+ * @class SubprocessError
+ * @brief Exception type thrown by execute().
+ */
+class SubprocessError : public std::exception
+{
+public:
+    SubprocessError(const std::string& msg) : msg_(msg) {}
+    ~SubprocessError() throw () {}
+
+private:
+    std::string msg_; ///< Error message.
+}; // class SubprocessError
+
+/**
+ * @brief Convert array of arguments to quoted string.
+ *
+ * @param [in] args Array of arguments.
+ *
+ * @returns Double quoted string, i.e., string where arguments are separated
+ *          by a space character and surrounded by double quotes if necessary.
+ *          Double quotes within an argument are escaped with a backslash.
+ *
+ * @sa split()
+ */
+std::string tostring(const std::vector<std::string>& args);
+
+/**
+ * @brief Split quoted string.
+ *
+ * @param [in] args Quoted string of arguments.
+ *
+ * @returns Array of arguments.
+ *
+ * @sa tostring()
+ */
+std::vector<std::string> split(const std::string& args);
+
+/**
+ * @brief Execute command as subprocess.
+ *
+ * This function is a replacement for system() on Unix and is furthermore
+ * less platform dependent. The first argument of the given command-line string
+ * is mapped to an absolute executable file using get_executable_path() if the
+ * given first argument is a know build target name. Otherwise, the command-line
+ * is used unmodified.
+ *
+ * @param [in] cmd         Command-line given as double quoted string. Arguments
+ *                         containing whitespaces have to be quoted using double
+ *                         quotes. Use a backslash (\\) to escape double quotes
+ *                         inside an argument as well as to escape a backslash
+ *                         itself (required if backslash at end of double quoted
+ *                         argument, e.g., "this argument \\").
+ * @param [in]  quiet      Turns off output of stdout of child process to stdout
+ *                         of parent process.
+ * @param [out] out        Output stream where command output is written to.
+ * @param [in]  allow_fail If true, no exception is thrown if the exit code
+ *                         of the child process is non-zero. Otherwise,
+ *                         a SubprocessException object is thrown in that case.
+ * @param [in]  verbose    Verbosity of output messages. Does not affect
+ *                         verbosity of executed command.
+ * @param [in]  simulate   Whether to simulate command execution only.
+ * @param [in]  targets    Structure providing information about executable targets.
+ *
+ * @returns Exit code of command or -1 if subprocess creation failed.
+ *
+ * @throws SubprocessError If subprocess creation failed or command returned
+ *                         a non-zero exit code while @p allow_fail is false.
+ */
+int execute(const std::string&           cmd,
+            bool                         quiet      = false,
+            // attention: stdout is a macro defined by windows.h
+            std::ostream*                out        = NULL,
+            bool                         allow_fail = false,
+            int                          verbose    = 0,
+            bool                         simulate   = false,
+            const IExecutableTargetInfo* targets    = NULL);
+
+/**
+ * @brief Execute command as subprocess.
+ *
+ * This function is a replacement for system() on Unix and is furthermore
+ * less platform dependent. The first argument of the given command-line string
+ * is mapped to an absolute executable file using get_executable_path() if the
+ * given first argument is a know build target name. Otherwise, the command-line
+ * is used unmodified.
+ *
+ * @param [in]  args       Command-line given as argument vector. The first
+ *                         argument has to be either a build target name or the
+ *                         name/path of the command to execute. Note that as a
+ *                         side effect, the first argument of the input vector
+ *                         is replaced by the absolute path of the actual
+ *                         executable file if applicable.
+ * @param [in]  quiet      Turns off output of stdout of child process to
+ *                         stdout of parent process.
+ * @param [out] out        Output stream where command output is written to.
+ * @param [in]  allow_fail If true, no exception is thrown if the exit code
+ *                         of the child process is non-zero. Otherwise,
+ *                         a SubprocessException object is thrown in that case.
+ * @param [in]  verbose    Verbosity of output messages. Does not affect
+ *                         verbosity of executed command.
+ * @param [in]  simulate   Whether to simulate command execution only.
+ * @param [in]  targets    Structure providing information about executable targets.
+ *
+ * @returns Exit code of command or -1 if subprocess creation failed.
+ *
+ * @throws SubprocessError If subprocess creation failed or command returned
+ *                         a non-zero exit code while @p allow_fail is false.
+ */
+int execute(std::vector<std::string>        args,
+            bool                            quiet      = false,
+            // attention: stdout is a macro defined by windows.h
+            std::ostream*                   out        = NULL,
+            bool                            allow_fail = false,
+            int                             verbose    = 0,
+            bool                            simulate   = false,
+            const IExecutableTargetInfo*    targets    = NULL);
+
+
+/// @}
+// end of Doxygen group
+
+
+} // namespace basis
+
+} // namespace sbia
+
+
+#endif // _SBIA_BASIS_UTILITIES_H
---
+++ src/utilities/cxx/utilities.cxx
@@ -0,0 +1,204 @@
+/**
+ * @file  utilities.cxx
+ * @brief Main module of project-independent BASIS utilities.
+ *
+ * Copyright (c) 2011, 2012 University of Pennsylvania. All rights reserved.<br />
+ * See http://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
+ *
+ * Contact: SBIA Group <sbia-software at uphs.upenn.edu>
+ */
+
+#include <sbia/basis/subprocess.h>
+#include <sbia/basis/utilities.h>
+
+
+// acceptable in .cxx file
+using namespace std;
+
+
+namespace sbia {
+
+namespace basis {
+
+
+// ===========================================================================
+// executable information
+// ===========================================================================
+
+// ---------------------------------------------------------------------------
+void print_contact(const char* contact)
+{
+    cout << "Contact:\n  " << contact << endl;
+}
+
+// ---------------------------------------------------------------------------
+void print_version(const char* name, const char* version, const char* project,
+                   const char* copyright, const char* license)
+{
+    assert(name    != NULL);
+    assert(version != NULL);
+    cout << name;
+    if (project && *project) cout << " (" << project << ")";
+    cout << " " << version << endl;
+    if (copyright && *copyright) cout << "Copyright (c) " << copyright << ". All rights reserved." << endl;
+    if (license && *license) cout << license << endl;
+}
+
+// ---------------------------------------------------------------------------
+string targetuid(const string& name, const IExecutableTargetInfo* targets)
+{
+    return targets != NULL ? targets->targetuid(name) : "";
+}
+
+// ---------------------------------------------------------------------------
+bool istarget(const string& name, const IExecutableTargetInfo* targets)
+{
+    return targets != NULL && targets->istarget(name);
+}
+
+// ---------------------------------------------------------------------------
+string exepath(const string& name, const IExecutableTargetInfo* targets)
+{
+    // return path of this executable if no name given
+    if (name.empty()) return os::exepath();
+    // get name of executable and check if target name is known
+    string exec_name = targets != NULL ? targets->basename(name) : "";
+    // if target is not known
+    if (exec_name.empty()) {
+        // return input argument assuming that it is already the path of
+        // an executable file
+        exec_name = name;
+        // try to get absolute path using the which command if path is relative
+        // TODO Replace use of external which command by C++ implementation
+        //      of which() even though BASIS includes a Python implementation
+        //      that can also be used on Windows. Still, a native C++
+        //      implementation is desireable.
+        if (!os::path::isabs(exec_name) && targets->istarget("sbia.basis.which")) {
+            vector<string> which(2);
+            which[0] = "sbia.basis.which";
+            which[1] = name;
+            ostringstream oss;
+            // attention: this includes a "recursive" call of this function!
+            if (execute(which, true, &oss, true, 0, false, targets) == 0) {
+                exec_name = oss.str();
+                const string::size_type end = exec_name.find_last_not_of(" \t\n\r");
+                if (end == string::npos) {
+                    exec_name = "";
+                } else {
+                    exec_name.erase(end + 1);
+                }
+            }
+        }
+        return exec_name;
+    }
+    return os::path::join(targets->dirname(name), exec_name);
+}
+
+// ---------------------------------------------------------------------------
+string exename(const std::string& name, const IExecutableTargetInfo* targets)
+{
+    string exec_path = exepath(name, targets);
+    if (exec_path.empty()) return "";
+#if WINDOWS
+    string ext = os::path::fileext(exec_path);
+    if (ext == ".exe" || ext == ".com") {
+        return os::path::filename(exec_path);
+    } else {
+        return os::path::basename(exec_path);
+    }
+#else
+    return os::path::basename(exec_path);
+#endif
+}
+
+// ---------------------------------------------------------------------------
+string exedir(const std::string& name, const IExecutableTargetInfo* targets)
+{
+    string exec_path = exepath(name, targets);
+    return exec_path.empty() ? "" : os::path::dirname(exec_path);
+}
+
+// ===========================================================================
+// command execution
+// ===========================================================================
+
+// ---------------------------------------------------------------------------
+string tostring(const vector<string>& args)
+{
+    return Subprocess::tostring(args);
+}
+
+// ---------------------------------------------------------------------------
+vector<string> split(const string& args)
+{
+    return Subprocess::split(args);
+}
+
+// ---------------------------------------------------------------------------
+int execute(const string& cmd, bool quiet, ostream* out,
+            bool allow_fail, int verbose, bool simulate,
+            const IExecutableTargetInfo* targets)
+{
+    vector<string> args = Subprocess::split(cmd);
+    return execute(args, quiet, out, allow_fail, verbose, simulate, targets);
+}
+
+// ---------------------------------------------------------------------------
+int execute(vector<string> args, bool quiet, ostream* out,
+            bool allow_fail, int verbose, bool simulate,
+            const IExecutableTargetInfo* targets)
+{
+    if (args.empty() || args[0].empty()) {
+        BASIS_THROW(SubprocessError, "execute_process(): No command specified");
+    }
+    // map build target name to executable file path
+    string exec_path = exepath(args[0], targets);
+    // prepend absolute path of found executable
+    if (!exec_path.empty()) args[0] = exec_path;
+    // some verbose output
+    if (verbose > 0) {
+        cout << "$ " << Subprocess::tostring(args);
+        if (simulate) cout << " (simulated)";
+        cout << endl;
+    }
+    // execute command
+    char buf[1024];
+    int  n;
+    int status = 0;
+    Subprocess p;
+    if (!p.popen(args, Subprocess::RM_NONE, Subprocess::RM_PIPE, Subprocess::RM_PIPE)) {
+        BASIS_THROW(SubprocessError, "execute_process(): Failed to create subprocess");
+    }
+    // read child's stdout (blocking)
+    if (!quiet || out != NULL) {
+        while ((n = p.read(buf, 1023)) > 0) {
+            buf[n] = '\0';
+            if (!quiet) {
+                cout << buf;
+                cout.flush();
+            }
+            if (out) *out << buf;
+        }
+    }
+    // wait for child process
+    if (!p.wait()) {
+        BASIS_THROW(SubprocessError, "execute_process(): Failed to wait for subprocess");
+    }
+    // write error messages to stderr of parent process
+    while ((n = p.read(buf, 1023, true)) > 0) {
+        buf[n] = '\0';
+        cerr << buf;
+    }
+    // get exit code
+    status = p.returncode();
+    // if command failed, throw an exception
+    if (status != 0 && !allow_fail) {
+        BASIS_THROW(SubprocessError, "Command " << Subprocess::tostring(args) << " failed");
+    }
+    return status;
+}
+
+
+} // namespace basis
+
+} // namespace sbia
