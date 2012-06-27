fde4cd702375cd445bdee1f551abeb8f16e344e7
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-06-27 04:35:01 +0000

    Change FindBASH.cmake modules to determine Bash version.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2454 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- FindBASH.cmake
+++ FindBASH.cmake
@@ -2,7 +2,9 @@
 # @file  FindBASH.cmake
 # @brief Find BASH interpreter.
 #
-# Sets the CMake variables @c BASH_FOUND and @c BASH_EXECUTABLE.
+# Sets the CMake variables @c BASH_FOUND, @c BASH_EXECUTABLE,
+# @c BASH_VERSION_STRING, @c BASH_VERSION_MAJOR, @c BASH_VERSION_MINOR, and
+# @c BASH_VERSION_PATCH.
 #
 # Copyright (c) 2012 University of Pennsylvania. All rights reserved.<br />
 # See http://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
@@ -16,6 +18,22 @@
 # find BASH executable
 find_program (BASH_EXECUTABLE bash)
 mark_as_advanced (BASH_EXECUTABLE)
+
+# ----------------------------------------------------------------------------
+# get version of found BASH executable
+if (BASH_EXECUTABLE)
+  execute_process (COMMAND "${BASH_EXECUTABLE}" --version OUTPUT_VARIABLE _BASH_STDOUT ERROR_VARIABLE _BASH_STDERR)
+  if (_BASH_STDOUT MATCHES "version ([0-9]+)\\.([0-9]+)\\.([0-9]+)")
+    set (BASH_VERSION_MAJOR "${CMAKE_MATCH_1}")
+    set (BASH_VERSION_MINOR "${CMAKE_MATCH_2}")
+    set (BASH_VERSION_PATCH "${CMAKE_MATCH_3}")
+    set (BASH_VERSION_STRING "${BASH_VERSION_MAJOR}.${BASH_VERSION_MINOR}.${BASH_VERSION_PATCH}")
+  else ()
+    message (WARNING "Failed to determine version of Bash interpreter (${BASH_EXECUTABLE})! Error:\n${_BASH_STDERR}")
+  endif ()
+  unset (_BASH_STDOUT)
+  unset (_BASH_STDERR)
+endif ()
 
 # ----------------------------------------------------------------------------
 # handle the QUIETLY and REQUIRED arguments and set *_FOUND to TRUE
