0e3ec7c3ef19fbaf09eae1028ef1453e13434fcc
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-07-16 05:02:43 +0000

    Add check of number of arguments to get_filename_component().
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2568 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- CommonTools.cmake
+++ CommonTools.cmake
@@ -490,10 +490,11 @@ endmacro ()
 # @sa http://www.cmake.org/cmake/help/cmake-2-8-docs.html#command:get_filename_component
 # @sa basis_get_filename_component()
 function (get_filename_component)
-  if (ARGC GREATER 4)
+  if (ARGC LESS 3)
+    message (FATAL_ERROR "[basis_]get_filename_component(): At least three arguments required!")
+  elseif (ARGC GREATER 4)
     message (FATAL_ERROR "[basis_]get_filename_component(): Too many arguments!")
   endif ()
-
   list (GET ARGN 0 VAR)
   list (GET ARGN 1 STR)
   list (GET ARGN 2 CMD)
