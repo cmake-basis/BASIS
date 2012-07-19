59f60c263295c6c2452fcd6c2170dd32f89cccf5
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-07-19 21:43:33 +0000

    Rename cProjectName to PROJECT.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2601 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- include/sbia/basis/testdriver.hxx
+++ include/sbia/basis/testdriver.hxx
@@ -220,9 +220,9 @@ int image_regression_test(const char*  imagefile,
         BASIS_THROW(runtime_error,
                     "Not implemented yet! Use ITK implementation instead, i.e.,"
                     << " install ITK 3.14 or greater (including versions after 4.0)"
-                    << " and reconfigure the build tree of " << cProjectName << ". Ensure that"
+                    << " and reconfigure the build tree of " << PROJECT << ". Ensure that"
                     << " the ITK_DIR variable is set to the directory of the ITKConfig.cmake file"
-                    << " and that the variable USE_ITK is set to ON. Then rebuild " << cProjectName
+                    << " and that the variable USE_ITK is set to ON. Then rebuild " << PROJECT
                     << " and optionally install it again.");
     #endif
 }
