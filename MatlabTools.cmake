071cd3e1a0581b3eb75dc38d7665dafcd562a90b
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-08-20 18:29:44 +0000

    Minor edit.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2750 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- MatlabTools.cmake
+++ MatlabTools.cmake
@@ -384,7 +384,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 \"${MATLAB_EXECUTABLE}\" -nodesktop -nosplash ${ARGN_OPTIONS} \\
-    -r \"try, addpath('${ARGN_MATLABPATH}','-begin'), ${ARGN_COMMAND}($args), catch err, fprintf(2, ['??? Error executing ${ARGN_COMMAND}\\n' err.message '\\n'), end, quit force\" \\
+    -r \"try, addpath('${ARGN_MATLABPATH}', '-begin'), ${ARGN_COMMAND}($args), catch err, fprintf(2, ['??? Error executing ${ARGN_COMMAND}\\n' err.message '\\n'), end, quit force\" \\
     2> >(tee \"$errlog\" >&2)"
   ) # end of file(WRITE) command
 endfunction ()
