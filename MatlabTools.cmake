1ad09ea16ba8ffd280e9142c37fc4c7b32651b51
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-08-20 18:03:01 +0000

    Escape backslashes in code of MATLAB executable wrapper.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2745 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- MatlabTools.cmake
+++ MatlabTools.cmake
@@ -372,8 +372,8 @@ while [[ $# -gt 0 ]]; do
   args=\"$args, '$1'\"
 done
 
-\"${MATLAB_EXECUTABLE}\" -nodesktop -nosplash ${ARGN_OPTIONS} \
-    -r \"try, addpath(${ARGN_MATLABPATH},'-begin'), ${ARGN_COMMAND}($args), catch err, fprintf(2, ['??? Error executing ${ARGN_COMMAND}\\n' err.message '\\n'), end, quit force\" \
+\"${MATLAB_EXECUTABLE}\" -nodesktop -nosplash ${ARGN_OPTIONS} \\
+    -r \"try, addpath(${ARGN_MATLABPATH},'-begin'), ${ARGN_COMMAND}($args), catch err, fprintf(2, ['??? Error executing ${ARGN_COMMAND}\\n' err.message '\\n'), end, quit force\" \\
     2> >(tee \"$errlog\" >&2)"
   ) # end of file(WRITE) command
 endfunction ()
