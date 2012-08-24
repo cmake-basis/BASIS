641c705a77a1233f3f245dedf5cffa74f53b4263
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-08-24 19:00:15 +0000

    Look also for files with .py extension (i.e., Windows) when looking for Sphinx tools.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2882 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- FindSphinx.cmake
+++ FindSphinx.cmake
@@ -78,7 +78,7 @@ endif ()
 # ----------------------------------------------------------------------------
 # find components, i.e., build tools
 foreach (_Sphinx_TOOL IN LISTS Sphinx_FIND_COMPONENTS)
-  find_program (Sphinx-${_Sphinx_TOOL}_EXECUTABLE NAMES sphinx-${_Sphinx_TOOL})
+  find_program (Sphinx-${_Sphinx_TOOL}_EXECUTABLE NAMES sphinx-${_Sphinx_TOOL} sphinx-${_Sphinx_TOOL}.py)
   mark_as_advanced (Sphinx-${_Sphinx_TOOL}_EXECUTABLE)
   list (APPEND _Sphinx_REQUIRED_VARS Sphinx-${_Sphinx_TOOL}_EXECUTABLE)
 endforeach ()
