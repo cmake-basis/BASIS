0e142bdb28458a256700bdc1b1979f5b166693d6
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-09-16 23:14:00 +0000

    Add Markdown version of README file used at SourceForge. Move script to update API links in documentation files to top directory.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@2973 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ update_apidoc_refs.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Small utility script used to update the links to the API documentation.
+# The documentation.rst file *must* be excluded, however, and updated manually.
+#
+# usage: update_apidoc_refs 2.0
+#
+# where the argument given, i.e., 2.0 in this example, is the current project version.
+
+release=$1
+if [ -z "${release}" ]; then
+    echo "No release version specified, e.g., 2.0!" 1>&2
+    exit 1
+fi
+[[ ${release} == 'latest' ]] || release=v${release}
+
+if [[ `printf ${BASH_VERSION} | grep 'apple'` ]]; then
+    find doc -type f \( -name '*.rst' ! -name documentation.rst \) -exec sed -i '' "s:/apidoc/v[0-9][0-9]*\.[0-9][0-9]*/:/apidoc/$release/:g;s:/apidoc/latest/:/apidoc/$release/:g" {} \;
+    sed -i '' "s:/apidoc/v[0-9][0-9]*\.[0-9][0-9]*/:/apidoc/$release/:g;s:/apidoc/latest/:/apidoc/$release/:g" README.md
+else
+    find doc -type f \( -name '*.rst' ! -name documentation.rst \) -exec sed -i'' "s:/apidoc/v[0-9][0-9]*\.[0-9][0-9]*/:/apidoc/$release/:g;s:/apidoc/latest/:/apidoc/$release/:g" {} \;
+    sed -i'' "s:/apidoc/v[0-9][0-9]*\.[0-9][0-9]*/:/apidoc/$release/:g;s:/apidoc/latest/:/apidoc/$release/:g" README.md
+fi
