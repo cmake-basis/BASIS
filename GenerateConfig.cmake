7ca781fde1355374c2a81a9cf839ab8d7c392ad3
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-09-20 09:26:46 +0000

    Use separate template package configuration files for modules.
    These template files exclude code which is only used by top-level projects.
    
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@3014 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- GenerateConfig.cmake
+++ GenerateConfig.cmake
@@ -92,14 +92,21 @@ endif ()"
 
 if (EXISTS "${PROJECT_CONFIG_DIR}/Config.cmake.in")
   set (TEMPLATE "${PROJECT_CONFIG_DIR}/Config.cmake.in")
+elseif (PROJECT_IS_MODULE)
+  set (TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/ModuleConfig.cmake.in")
 else ()
   set (TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in")
 endif ()
 
 # ----------------------------------------------------------------------------
 # provide code of BASIS config file as variable
-if (NOT "${TEMPLATE}" STREQUAL "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in")
-  file (READ "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in" BASIS_TEMPLATE)
+
+if (NOT TEMPLATE MATCHES "^${CMAKE_CURRENT_LIST_DIR}/")
+  if (PROJECT_IS_MODULE)
+    file (READ "${CMAKE_CURRENT_LIST_DIR}/ModuleConfig.cmake.in" BASIS_TEMPLATE)
+  else ()
+    file (READ "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in"       BASIS_TEMPLATE)
+  endif ()
   # remove file header
   string (REGEX REPLACE "^########.*########" "" BASIS_TEMPLATE "${BASIS_TEMPLATE}")
   string (STRIP "${BASIS_TEMPLATE}" BASIS_TEMPLATE)
@@ -179,14 +186,20 @@ if (EXISTS "${PROJECT_CONFIG_DIR}/ConfigUse.cmake.in")
 elseif (EXISTS "${PROJECT_CONFIG_DIR}/Use.cmake.in")
   # backwards compatibility to version <= 0.1.8 of BASIS
   set (TEMPLATE "${PROJECT_CONFIG_DIR}/Use.cmake.in")
+elseif (PROJECT_IS_MODULE)
+  set (TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/ModuleConfigUse.cmake.in")
 else ()
   set (TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/ConfigUse.cmake.in")
 endif ()
 
 # ----------------------------------------------------------------------------
 # provide code of BASIS use file as variable
-if (NOT "${TEMPLATE}" STREQUAL "${CMAKE_CURRENT_LIST_DIR}/ConfigUse.cmake.in")
-  file (READ "${CMAKE_CURRENT_LIST_DIR}/ConfigUse.cmake.in" BASIS_USE)
+if (NOT TEMPLATE MATCHES "^${CMAKE_CURRENT_LIST_DIR}/")
+  if (PROJECT_IS_MODULE)
+    file (READ "${CMAKE_CURRENT_LIST_DIR}/ModuleConfigUse.cmake.in" BASIS_USE)
+  else ()
+    file (READ "${CMAKE_CURRENT_LIST_DIR}/ConfigUse.cmake.in"       BASIS_USE)
+  endif ()
   # remove file header
   string (REGEX REPLACE "^########.*########" "" BASIS_USE "${BASIS_USE}")
   string (STRIP "${BASIS_USE}" BASIS_USE)
