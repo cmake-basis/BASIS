2c87acdc48f1184b33ce90c01cc38292925a5ead
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-10-30 21:58:22 +0000

    Fix relative paths in executable target info tables.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@3089 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- UtilitiesTools.cmake
+++ UtilitiesTools.cmake
@@ -593,14 +593,9 @@ function (_basis_generate_executable_target_info CXX PYTHON PERL BASH)
       if (INSTALL_LOCATION AND (NOT IMPORTED OR BUNDLED))
         file (
           RELATIVE_PATH INSTALL_LOCATION_REL2${L}
-            "${CMAKE_INSTALL_PREFIX}/${INSTALL_${L}_DIR}"
+            "${CMAKE_INSTALL_PREFIX}/${INSTALL_${L}_DIR}/<package>"
             "${INSTALL_LOCATION}"
         )
-        if (NOT INSTALL_LOCATION_REL2${L})
-          set (INSTALL_LOCATION_REL2${L} "../..")
-        else ()
-          set (INSTALL_LOCATION_REL2${L} "../../${INSTALL_LOCATION_REL2${L}}")
-        endif ()
       else ()
         set (INSTALL_LOCATION_REL2${L} "${INSTALL_LOCATION}")
       endif ()
