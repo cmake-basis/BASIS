69f31d1d0c0dbbdef03e3822564d0e3277942a33
Andreas Schuh <andreas.schuh.84@gmail.com>
2013-05-21 22:20:26 +0000

    Fix issue with future statements in Python scripts. Fix removal of more than one blank line as substitute for inserted additional code to scripts.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@3155 3c61e6fb-ae43-4039-8cf7-c895b3257837

---
+++ test/cmake/test_future_statements.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+
+# Some comment line
+
+"""
+
+ A doc string which is
+   allowed before the future statements...
+   
+  This Python script is used to test the basis_configure_script
+  CMake function which adds Python code to modify the search
+  path for modules to the top of the script. This code must be
+  inserted below all future statements, however!
+  
+"""
+
+from __future__ import print_function
+from __future__ import division
+
+
+from __future__ import generators
+from __future__ import unicode_literals
+
+
+
+from __future__ import with_statement
+
+
+
+from basis import utilities
+
+
+
