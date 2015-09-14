bb52c45cb0cc1590e3a62611f5aad5437bd5517a
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-09-14 11:58:10 +0100

    #294 Set PATH and LD_LIBRARY_PATH in build script

--- test/build/basis.sh
+++ test/build/basis.sh
@@ -10,6 +10,11 @@ set -e
 [ -n "$doc"       ] || doc=no
 [ -n "$tests"     ] || tests=no
 
+# Use dependencies built and installed from sources
+export PATH="$prefix/bin:$PATH"
+[[ $TRAVIS_OS_NAME != linux ]] || export   LD_LIBRARY_PATH="$prefix/lib:$LD_LIBRARY_PATH"
+[[ $TRAVIS_OS_NAME != osx   ]] || export DYLD_LIBRARY_PATH="$prefix/lib:$DYLD_LIBRARY_PATH"
+
 # Configure build
 cmake -DBUILD_TESTING=$tests \
       -DBUILD_DOCUMENTATION=$doc \
