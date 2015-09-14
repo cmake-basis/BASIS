70daca3f9aca1d7a6342913d28e6f50670fe12f7
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-09-14 12:17:55 +0100

    #294 Fix BASIS respository installation and use CMake binary packages

--- test/build/cmake.sh
+++ test/build/cmake.sh
@@ -11,16 +11,37 @@ if [[ $version == any ]]; then
   [[ $TRAVIS_OS_NAME != osx   ]] || exec brew install cmake
 fi
 
-# Download and extract source files
-wget http://www.cmake.org/files/v${version%.*}/cmake-${version}.tar.gz
-tar xzf cmake-${version}.tar.gz
+if [[ $TRAVIS_OS_NAME == linux ]]; then
 
-# Configure build
-cd cmake-$version
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX="$prefix" \
-      ..
+  # Download and extract binary distribution package
+  wget http://www.cmake.org/files/v${version%.*}/cmake-${version}-Linux-x86_64.tar.gz
+  tar --strip-components 1 -C "$prefix" -xzf cmake-${version}-Linux-x86_64.tar.gz
 
-# Build and install
-make install
+elif [[ $TRAVIS_OS_NAME == osx ]]; then
+
+  # Download and extract binary distribution package
+  wget http://www.cmake.org/files/v${version%.*}/cmake-${version}-Darwin-x86_64.tar.gz
+  tar -xzf cmake-${version}-Darwin-x86_64.tar.gz
+
+  # Copy extracted files to installation prefix
+  for d in bin doc man share; do
+    mkdir -p "$prefix/$d" && mv -f "cmake-${version}-Darwin-x86_64/$d/*" "$prefix/$d/"
+  done
+
+else
+
+  # Download and extract source files
+  wget http://www.cmake.org/files/v${version%.*}/cmake-${version}.tar.gz
+  tar xzf cmake-${version}.tar.gz
+  
+  # Configure build
+  cd cmake-$version
+  mkdir build && cd build
+  cmake -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$prefix" \
+        ..
+  
+  # Build and install
+  make install
+
+fi
