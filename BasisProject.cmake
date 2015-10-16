0d3b46b51a84a87e56718eb398119c2c9214d89e
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-10-16 22:04:11 +0100

    #451 Change version to 3.2.0 (again)

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -83,7 +83,7 @@ basis_project (
   # --------------------------------------------------------------------------
   # meta-data
   NAME          "BASIS"
-  VERSION       "0.0.0"
+  VERSION       "3.2.0"
   AUTHORS       "Andreas Schuh"
   DESCRIPTION   "This package implements and supports the development of "
                 "software which follows the CMake Build system And Software "
