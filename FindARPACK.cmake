8c1802fdf164a2bd21afa685b660f835993368f3
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-01-03 01:37:38 +0000

    enh: Improof FindUMFPACK.cmake and FindARPACK.cmake modules

--- FindARPACK.cmake
+++ FindARPACK.cmake
@@ -1,15 +1,15 @@
 find_library(ARPACK_LIBRARY
   NAMES parpack arpack
-  PATHS ${ARPACK_DIR}
+  PATHS ${ARPACK_DIR}/lib
 )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ARPACK DEFAULT_MSG ARPACK_LIBRARY)
 
-if(ARPACK_FOUND)
+if (ARPACK_FOUND)
   get_filename_component(ARPACK_DIR ${ARPACK_LIBRARY} PATH)
-  set(ARPACK_INCLUDE_DIR ${ARPACK_DIR}/../include CACHE FILEPATH "ARPACK include directory.")
-  mark_as_advanced(ARPACK_LIBRARY ARPACK_INCLUDE_DIR)
-else()
-  mark_as_advanced(CLEAR ARPACK_LIBRARY)
-endif()
+  get_filename_component(ARPACK_DIR ${ARPACK_DIR}     PATH)
+  set(ARPACK_INCLUDE_DIR ${ARPACK_DIR}/include CACHE FILEPATH "ARPACK include directory.")
+  mark_as_advanced(ARPACK_INCLUDE_DIR)
+endif ()
+mark_as_advanced(ARPACK_LIBRARY)
